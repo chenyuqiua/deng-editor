489937dbaa8145b2dc9d6f5f426592e582e2746b
dengzhengcheng <lananabc@163.com>
2025-07-18 10:31:51 +0800

    feat: add asset for test

--- src/lib/remotion/test-component.tsx
+++ src/lib/remotion/test-component.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react'
-import { AbsoluteFill, Img, Sequence, useCurrentFrame } from 'remotion'
+import { AbsoluteFill, Img, OffthreadVideo, Sequence, staticFile, useCurrentFrame } from 'remotion'
 
 export const TestComponent = memo(() => {
   const frame = useCurrentFrame()
@@ -12,7 +12,7 @@ export const TestComponent = memo(() => {
         backgroundColor: 'white',
       }}
     >
-      <Sequence from={20} durationInFrames={21}>
+      <Sequence from={0} durationInFrames={21}>
         The current frame is {frame}.
         <div
           style={{
@@ -24,7 +24,25 @@ export const TestComponent = memo(() => {
         >
           <Img
             draggable={false}
-            src={'https://cdn.pixabay.com/photo/2025/06/10/11/21/view-9651981_640.jpg'}
+            src={staticFile('/image/test2.jpg')}
+            style={{
+              userSelect: 'none',
+              position: 'relative',
+            }}
+          />
+        </div>
+      </Sequence>
+      <Sequence from={20} durationInFrames={21}>
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+            position: 'relative',
+          }}
+        >
+          <OffthreadVideo
+            src={staticFile('/video/test1.mp4')}
             style={{
               userSelect: 'none',
               position: 'relative',
