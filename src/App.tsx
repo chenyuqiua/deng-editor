60f63ec0aea34e08e8ed0dd2789490fc7f1d685d
dengzhengcheng <lananabc@163.com>
2025-07-27 16:46:49 +0800

    feat(timeline): add play btn and time

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
   return (
-    <IconParkProvider jsUrl="https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_40263_79.b132e7fe5f1aa6d9acaf1bae401e1b95.js">
+    <IconParkProvider jsUrl="https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_40263_85.adf9bcad7cf5aff6219a833a88c373ff.js">
       <DndProvider backend={HTML5Backend}>
         <EditorMainPage />
       </DndProvider>
--- src/page/editor/timeline/block/timeline-action.tsx
+++ src/page/editor/timeline/block/timeline-action.tsx
@@ -5,11 +5,17 @@ import { useZustand } from 'use-zustand'
 import { useDraftSelector } from '../../hook/draft'
 import _ from 'lodash'
 import { useSize } from '@/common/hook/use-size'
+import { Button } from '@/component/ui/button'
+import { usePlayerSelector } from '../../hook/player'
+import { format, subHours } from 'date-fns'
+import { getPlayerService } from '../../util/service'
 
 export const TimelineAction = memo(() => {
   const vc = useTimelineViewController()
   const pixelPerSecond = useZustand(vc.store, s => s.pixelPerSecond)
   const duration = useDraftSelector(s => s.duration)
+  const playerService = getPlayerService()
+  const [isPlaying, currentTime] = usePlayerSelector(s => [s.isPlaying, s.currentTime])
   const scaleDomRef = useRef<HTMLDivElement>(vc.scaleDom)
   const { width: scaleWidth } = useSize(scaleDomRef) || { width: 0 }
   // 当前刻度尺宽度能够刚好容下的时间长度
@@ -25,7 +31,23 @@ export const TimelineAction = memo(() => {
   const throttleUpdatePixelPerSecond = _.throttle(vc.updatePixelPerSecond.bind(vc), 100)
 
   return (
-    <div className="flex h-12 items-center">
+    <div className="flex h-12 items-center justify-between px-3">
+      <div className="w-[150px]"></div>
+      <div className="flex flex-1 items-center justify-center gap-2">
+        <Button
+          icon={isPlaying ? 'pause' : 'play'}
+          iconSize={20}
+          variant="secondary"
+          className="text-white"
+          onClick={() => {
+            playerService.toggle()
+          }}
+        />
+        <div className="text-label-sm text-white">
+          {format(subHours(new Date(currentTime * 1000), 8), 'HH:mm:ss')}/
+          {format(subHours(new Date(duration * 1000), 8), 'HH:mm:ss')}
+        </div>
+      </div>
       <div className="flex w-[150px] items-center">
         <Slider
           // 最小值按照刻度尺的宽度计算, 目的是让其最小时, 刻度尺刚好完整展示
